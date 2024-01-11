5aa08f96a75add48034fc4d7ce675dbf3091fad8
Miracle-229 <zzzzhd02@gmail.com>
2024-01-11 15:30:55 +0300

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({
-    include: "**/*.svg?react",
-  })],
-  server: {
-    port: 5173,
-    host: '127.0.0.1',
-  },
-  base: '/AZATI_Workshop/',
-});
+	plugins: [
+		react(),
+		svgr({
+			include: '**/*.svg?react'
+		})
+	],
+	server: {
+		port: 5173,
+		host: '127.0.0.1'
+	},
+	base: '/AZATI_Workshop_test/'
+})
